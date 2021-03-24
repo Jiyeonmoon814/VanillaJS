80104a430f2360a9f36f4dd59f26ff2004ac2012
JiyeonMoon <jiyeonmoon814@gmail.com>
2021-03-24 19:30:33 +0900

    add filtertodo fx

--- simpleTodoList/app.js
+++ simpleTodoList/app.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList= document.querySelector('.todo-list');
+const filterOpt = document.querySelector('.filter-todo');
 
 //Event Listeners
 todoButton.addEventListener('click',addTodo);
 todoList.addEventListener('click',deleteCheck);
+filterOpt.addEventListener('click',filterTodo);
 
 
 //Functions
@@ -51,12 +53,42 @@ function deleteCheck(e){
     //delete todo
     if(clickedItem.classList[0]==="delete-btn"){
         const todo = clickedItem.parentElement;
-        todo.remove();
+        //animation
+        todo.classList.add("fall");
+        todo.addEventListener('transitionend',function(){
+            todo.remove();
+        });
     }
 
     if(clickedItem.classList[0]==="complete-btn"){
         const todo = clickedItem.parentElement;
         todo.classList.toggle("completed");
     }
+}
+
+function filterTodo(e){
+    const todos = todoList.childNodes;
+    //console.log(todos);
+    todos.forEach(function(todo){
+        switch(e.target.value){
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if(todo.classList.contains("completed")){
+                    todo.style.display = "flex";
+                }else{
+                    todo.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if(!todo.classList.contains("completed")){
+                    todo.style.display = "flex";
+                }else{
+                    todo.style.display = "none";
+                }
+                break;
+        }
+    });
 }
 
