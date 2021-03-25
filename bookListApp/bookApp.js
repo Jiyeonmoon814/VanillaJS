796033351b64678bcb2665bccc5c9c6efb3cd761
JiyeonMoon <jiyeonmoon814@gmail.com>
2021-03-25 19:46:33 +0900

    add fuction in the class

--- bookListApp/bookApp.js
+++ bookListApp/bookApp.js
@@ -31,15 +31,47 @@ class UI {
         const list = document.querySelector('#book-list');
 
         const row = document.createElement('tr');
+        row.innerHTML = //'
+        //<td>${book.title}</td>
+        //<td>${book.author}</td>
+        //<td>${book.isbn}</td>
+        //<td><a href="#" class="btn btn-outline-primary btn-sm delete">X</a></td>
+        //';
+        '<td>${book.title}</td><td>${book.author}</td><td>${book.isbn}</td><td><a href="#" class="btn btn-outline-primary btn-sm delete">X</a></td>';
+        
+        list.appendChild(row);
+    }
 
-        row.innerHTML 
-        = '<td>{$book.title}</td><td>{$book.author}</td><td>{$book.isbn}</td><td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>';
+    static clearField() {
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#isbn').value = '';
     }
 }
 //Store Class : Handles Storage
 
 //Event : Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 //Event : Add a Book
+document.querySelector('#book-form').addEventListener('submit',(e) => {
+        e.preventDefault();
+        //Get form Values
+        const title = document.querySelector('#title').value;
+        const author = document.querySelector('#author').value;
+        const isbn = document.querySelector('#isbn').value;
+
+        //Instatiate book
+        const book = new Book(title, author, isbn);
+
+        //console.log(book);
+
+        //Add book to UI
+        UI.addBookToList(book);
+
+        //Clear Field
+        UI.clearField();
+
+    });
 
 //Event : Remove a Book
