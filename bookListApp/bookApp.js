b53bc363865c5c0735632492869eb9c7127f6f46
JiyeonMoon <jiyeonmoon814@gmail.com>
2021-03-25 19:11:13 +0900

    add book class

--- bookListApp/bookApp.js
+++ bookListApp/bookApp.js
@@ -1,7 +1,41 @@
 //Book Class >> Represents a Book
-
+class Book {
+    constructor(title, author, isbn){
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
 //UI Class : Handle UI Tasks
+class UI {
+    static displayBooks() {
+        const StoredBooks = [
+            {
+                title : 'Harry Potter',
+                author : 'J.K Rowling',
+                isbn : '123456'
+            },
+            {
+                title : 'The Vegetarian',
+                author : 'Han Kang',
+                isbn : '987654'
+            }
+        ];
 
+        const books = StoredBooks;
+
+        books.forEach((book) => UI.addBookToList(book));
+    }
+
+    static addBookToList(book) {
+        const list = document.querySelector('#book-list');
+
+        const row = document.createElement('tr');
+
+        row.innerHTML 
+        = '<td>{$book.title}</td><td>{$book.author}</td><td>{$book.isbn}</td><td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>';
+    }
+}
 //Store Class : Handles Storage
 
 //Event : Display Books
