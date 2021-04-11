dd088e0f633394d6704900fa1449943f4a1e14d3
JiyeonMoon <jiyeonmoon814@gmail.com>
2021-04-11 14:05:06 +0900

    add TypeWriter.prototype.type function

--- TypeWriteEffect/app.js
+++ TypeWriteEffect/app.js
@@ -3,15 +3,51 @@ const TypeWriter = function(txtElement, words, wait = 3000) {
     this.words = words;
     this.txt =''; // D,e,v ... 
     this.wordIndex = 0; //for an array
-    this.wait = parseInt(wait,10);
+    this.wait = parseInt(wait,7);
     this.type();
     this.isDeleting = false; //represent a state
 }
 
 // Type Method
 TypeWriter.prototype.type = function(){
-    //console.log('hello');
-    setTimeout(()=>this.type(),500)
+    //Current index of word
+    const current = this.wordIndex % this.words.length; //0
+    //Get full text of current word
+    const fulltxt = this.words[current]; //FrontEnd
+
+    //check if deleting
+    if(this.isDeleting){
+        //Remove char
+        this.txt = fulltxt.substring(0,this.txt.length-1);
+    }else {
+        //Add char
+        this.txt = fulltxt.substring(0,this.txt.length+1);
+    }
+
+    //Insert txt into element
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+    
+    //Initial Type Speed
+    let typeSpeed = 300;
+
+    if(this.isDeleting) {
+        typeSpeed /= 2; //TypeSpeed = TypeSeed / 2
+    }
+
+    //If word is complete
+    if(!this.isDeleting && this.txt === fulltxt){
+        //Make pause at end
+        typeSpeed = this.wait;
+        //Set delete to true
+        this.isDeleting = true;
+    }else if(this.isDeleting && this.txt ===''){
+        this.isDeleting = false;
+        //Move to next word
+        this.wordIndex++;
+        //Pause beore start typing
+        typeSpeed = 500;
+    }
+    setTimeout(()=>this.type(), typeSpeed)
 }
 //Init on DOM Load
 document.addEventListener('DOMContentLoaded',init);
