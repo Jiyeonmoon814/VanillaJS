41e9c2575767cc368bc6237fb76a526d3fc0ca06
JiyeonMoon <jiyeonmoon814@gmail.com>
2021-06-06 23:21:18 +0900

    declare dom elements

--- passwordGenerator/pwdGenerator.js
+++ passwordGenerator/pwdGenerator.js
@@ -1,4 +1,21 @@
-//Generator random lower,uppercase and numbers 
+//DOM elements
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
+
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+}
+
+//Generator random lower,uppercase,numbers and symbols
 //from character set (such as a>>97,A>>65)
 function getRandomLower(){
     //print lowercase letters(from 97-122)
@@ -14,4 +31,11 @@ function getRandomNumber(){
     //print number letters(from 49-57)
     return String.fromCharCode(Math.floor(Math.random()*10)+48);
 }
+
+function getRandomSymbol(){
+    const symbols = '!@#$%^&*(){}[]=<>/,.';
+    return symbols[Math.floor(Math.random()*symbols.length)];
+}
+
+console.log(getRandomSymbol())
 
