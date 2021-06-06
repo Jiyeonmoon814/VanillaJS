33388ef0541fd9a0122f5fc7e9631bc5fa00574f
JiyeonMoon <jiyeonmoon814@gmail.com>
2021-06-06 23:44:16 +0900

    generate pwd func

--- passwordGenerator/pwdGenerator.js
+++ passwordGenerator/pwdGenerator.js
@@ -23,10 +23,34 @@ generateEl.addEventListener('click',()=>{
     const hasUpper = uppercaseEl.checked;
     const hasNumber = numbersEl.checked;
     const hasSymbol = symbolsEl.checked;
-    
+
     resultEl.innerText=generatePassword(hasLower,hasUpper,hasNumber,hasSymbol,length);
 })
 
+//Generate password function 
+function generatePassword(lower,upper,number,symbol,length){
+    //1.Init pwd var 
+    let generatedPwd = '';
+    
+    //2.Filter out unchecked types 
+    const typesCount = lower + upper + number + symbol;
+    const typesArr = [{lower},{upper},{number},{symbol}].filter(item=>Object.values(item)[0]);
+
+    if(typesCount===0){
+        return '';
+    }
+    
+    //3.Loop over length call generator function for each type 
+    for(let i=0;i<length;i+=typesCount){
+        typesArr.forEach(type=>{
+            const funcName = Object.keys(type)[0];
+            generatedPwd += randomFunc[funcName]();
+        })
+    }
+    //4.Add final pwd to the pwd variable and return it 
+    const finalPwd = generatedPwd.slice(0,length);
+    return finalPwd;
+}
 //Functions 
 //Generator random lower,uppercase,numbers and symbols
 //from character set (such as a>>97,A>>65)
@@ -50,5 +74,4 @@ function getRandomSymbol(){
     return symbols[Math.floor(Math.random()*symbols.length)];
 }
 
-console.log(getRandomSymbol())
 
