c5d22af386565b91917e6c0c3d7f4860661b5993
JiyeonMoon <jiyeonmoon814@gmail.com>
2021-06-13 15:53:31 +0900

    add play/pause func and event listener

--- musicPlayer/musicPlayer.js
+++ musicPlayer/musicPlayer.js
@@ -21,5 +21,34 @@ loadSong(songs[songIndex])
 function loadSong(song){
     title.innerHTML = song
     audio.src = `music/${song}.mp3`
-    cover.src = `image/${song}.jpg`
+    cover.src = `images/${song}.jpg`
 }
+
+//change a play icon to a puse icon or the other way around
+//depending on whether className is paly or pause 
+function playSong(){
+    musicContainer.classList.add('play')
+    playBtn.querySelector('i.fas').classList.remove('fa-play')
+    playBtn.querySelector('i.fas').classList.add('fa-pause')
+
+    audio.play()
+}
+
+function pauseSong(){
+    musicContainer.classList.remove('play')
+    playBtn.querySelector('i.fas').classList.remove('fa-pause')
+    playBtn.querySelector('i.fas').classList.add('fa-play')
+
+    audio.pause()
+}
+
+//Event Listeners 
+playBtn.addEventListener('click', () =>{
+    const isPlaying = musicContainer.classList.contains('play')
+
+    if(isPlaying){
+        pauseSong()
+    }else {
+        playSong()
+    }
+})
