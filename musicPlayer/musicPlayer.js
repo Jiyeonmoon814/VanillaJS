7f771d3e6b05f66736325ae6bcc3124ffbee02f6
JiyeonMoon <jiyeonmoon814@gmail.com>
2021-06-13 17:48:28 +0900

    add update&set progress bar func

--- musicPlayer/musicPlayer.js
+++ musicPlayer/musicPlayer.js
@@ -42,6 +42,47 @@ function pauseSong(){
     audio.pause()
 }
 
+//change songs func
+function prevSong() {
+    songIndex--
+
+
+    //If the song is less than 0, I want to loop to the beginning 
+    if(songIndex<0){
+        songIndex = songs.length - 1
+    }
+
+    loadSong(songs[songIndex])
+
+    playSong()
+}
+
+function nextSong() {
+    songIndex++
+
+    if(songIndex > songs.length-1){
+        songIndex = 0
+    }
+
+    loadSong(songs[songIndex])
+
+    playSong()
+}
+
+function updateProgress(e){
+    const {duration, currentTime} = e.srcElement
+    const progressPercent = (currentTime/duration) * 100
+    progress.style.width = `${progressPercent}%`
+}
+
+function setProgress(e) {
+    const width = this.clientWidth
+    const clickX = e.offsetX 
+    const duration = audio.duration
+
+    audio.currentTime = (clickX/width) * duration
+}
+
 //Event Listeners 
 playBtn.addEventListener('click', () =>{
     const isPlaying = musicContainer.classList.contains('play')
@@ -52,3 +93,13 @@ playBtn.addEventListener('click', () =>{
         playSong()
     }
 })
+
+//Change song events 
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
+
+audio.addEventListener('timeupdate', updateProgress)
+
+progressContainer.addEventListener('click', setProgress)
+
+audio.addEventListener('ended', nextSong)
