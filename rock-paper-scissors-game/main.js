3454f7b341f55bf57a62c7989dbed1d6cc3c9d39
JiyeonMoon <jiyeonmoon814@gmail.com>
2021-07-04 17:02:54 +0900

    add showWinner and restart func

--- rock-paper-scissors-game/main.js
+++ rock-paper-scissors-game/main.js
@@ -14,6 +14,7 @@ function play(e) {
     const playerChoice = e.target.id;
     const computerChoice = getComputerChoice();
     const winner = getWinner(playerChoice, computerChoice);
+    showWinner(winner,computerChoice);
 }
 
 //Get computers choice 
@@ -28,5 +29,90 @@ function getComputerChoice() {
     }
 }
 
+//Get game winner 
+function getWinner(p,c){
+    if(p===c){
+        return 'draw';
+    }else if(p==='rock'){
+        if(c==='paper'){
+            return 'computer'
+        }else {
+            return 'player'
+        }
+    }else if(p==='paper'){
+        if(c==='scissors'){
+            return 'computer'
+        }else {
+            return 'player'
+        }
+    }else if(p==='scissors'){
+        if(c==='rock'){
+            return 'computer'
+        }else{
+            return 'player'
+        }
+    }
+}
+
+//Show winner 
+function showWinner(winner, computerChoice){
+    if(winner==='player'){
+        //Increment player's score
+        scoreboard.player++;
+        // Show modal result 
+        result.innerHTML = `
+        <h1 class="text-win">You Win</h1>
+        <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+            <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase()
+            + computerChoice.slice(1)}</strong></p>
+        `;
+    }else if(winner==='computer') {
+        //Increment computer's score
+        scoreboard.computer++;
+        // Show modal result 
+        result.innerHTML = `
+        <h1 class="text-lose">You Lose</h1>
+        <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+            <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase()
+            + computerChoice.slice(1)}</strong></p>
+        `;
+    }else {
+        // Show modal result 
+        result.innerHTML = `
+        <h1>It's a Draw</h1>
+        <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+            <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase()
+            + computerChoice.slice(1)}</strong></p>
+        `;
+    }
+
+    //Show score
+    score.innerHTML = `
+    <p>Player: ${scoreboard.player}</p>
+    <p>Computer: ${scoreboard.computer}</p>
+    `;
+
+    modal.style.display = 'block';
+}
+
+//Restart game
+function restartGame() {
+    scoreboard.player = 0;
+    scoreboard.computer = 0;
+    score.innerHTML = `
+    <p>Player: 0</p>
+    <p>Computer: 0</p>
+    `;
+    restart.style.display='none';
+}
+//Clear Modal
+function clearModal(e){
+    if(e.target===modal){
+        modal.style.display='none'
+    }
+}
+
 //event listeners 
 choices.forEach(choice => choice.addEventListener('click',play));
+window.addEventListener('click',clearModal);
+restart.addEventListener('click',restartGame)
