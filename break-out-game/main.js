5b62da00fe70af8d54d68e7c6df5b79b5dbf25d9
JiyeonMoon <jiyeonmoon814@gmail.com>
2021-07-10 17:36:39 +0900

    move ball and paddle and increae score

--- break-out-game/main.js
+++ break-out-game/main.js
@@ -24,7 +24,7 @@ let score = 0
 
 const brickRowCount = 9;
 const brickColumnCount = 5;
-const delay = 500; //delay to reset the game
+const delay = 1000; //delay to reset the game
 
 //Create ball props 
 const ball = {
@@ -101,6 +101,108 @@ function drawBricks() {
     })
 }
 
+//Draw score on canvas
+function drawScore(){
+    ctx.font = '20px Arial'
+    ctx.fillText =(`Score:${score}`, canvas.width - 100, 30)
+}
+
+//Move paddle on canvas 
+function movePaddle() {
+    paddle.x += paddle.dx;
+
+    //Wall detection
+    if(paddle.x + paddle.w > canvas.width){
+        paddle.x = canvas.width - paddle.w;
+    }
+
+    if(paddle.x<0){
+        paddle.x = 0;
+    }
+}
+
+//Move ball on canvas
+function moveBall(){
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+
+    //Wall collision (right/left)
+    if(ball.x + ball.size > canvas.width || ball.x - ball.size < 0){
+        ball.dx *= -1; // ball.dx = ball.dx*-1
+    }
+
+    //Wall collision (top/bottom)
+    if(ball.y+ball.size > canvas.height || ball.y - ball.size < 0){
+        ball.dy *= -1;
+    }
+    
+    //Paddle collision
+    if(
+        ball.x - ball.size > paddle.x &&
+        ball.x + ball.size < paddle.x + paddle.w &&
+        ball.y + ball.size > paddle.y
+    ){
+        ball.dy = -ball.speed
+    }
+
+    //Brick collision
+    bricks.forEach(column => {
+        column.forEach(brick => {
+            if(brick.visible){
+                if(
+                    ball.x - ball.size > brick.x && //left brick side check
+                    ball.x + ball.size < brick.x + brick.w && //right brick side check
+                    ball.y + ball.size > brick.y && //top brick side check
+                    ball.y - ball.size < brick.y + brick.h //bottom side check
+                ){
+                    ball.dy *= -1;
+                    brick.visible = false;
+
+                    increaseScore();
+                }
+            }
+        })
+    })
+
+    //Hit bottom wall - Lose 
+    if(ball.y + ball.size > canvas.height){
+        showAllBricks();
+        score =0;
+    }
+}
+
+//Increase score
+function increaseScore(){
+    score++;
+
+    if(score % (brickRowCount * brickColumnCount)===0){
+        ball.visible = false;
+        paddle.visible = false;
+
+        //After 0.5sec restart the game
+
+        setTimeout(function(){
+            showAllBricks();
+            score=0;
+            paddle.x = canvas.width / 2 - 40;
+            paddle.y = canvas.height - 20;
+            ball.x = canvas.width / 2;
+            ball.y = canvas.height / 2;
+            ball.visible = true;
+            paddle.visible = true;
+        }, delay)
+    }
+}
+
+//Make all bricks appear 
+function showAllBricks(){
+    bricks.forEach(column => {
+        column.forEach(brick => 
+                (brick.visible=true)
+            )
+    })
+}
+
 //Draw everything
 function draw(){
     //need to clear canavs first 
@@ -109,17 +211,45 @@ function draw(){
     drawBall();
     drawPaddle();
     drawBricks();
+    drawScore();
 }
 
 //Update cavnas drawing and animation
 function update(){
+    //Move paddle and ball
+    movePaddle()
+    moveBall()
+
     //draw everything
     draw()
+
+    requestAnimationFrame(update)
 }
 
+
+//key down event 
+function keyDown(e) {
+    if(e.key==='Right' || e.key === 'ArrowRight'){
+        paddle.dx = paddle.speed;
+    }else if(e.key==='Left' || e.key === 'ArrowLeft'){
+        paddle.dx = -paddle.speed;
+    }
+}
+
+//key up event
+function keyUp(e){
+    if(
+        e.key==='Right' ||
+        e.key==='ArrowRight' ||
+        e.key==='Left' ||
+        e.key==='ArrowLeft'
+    ){
+        paddle.dx = 0;
+    }
+}
 // Keyboard event handlers
-//document.addEventListener('keydown', keyDown);
-//document.addEventListener('keyup', keyUp);
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
 
 //Rules and close event handlers 
 rulesBtn.addEventListener('click', () => 
