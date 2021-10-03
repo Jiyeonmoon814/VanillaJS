3bfd8d8ac30f0df14068cab67d2ad34285f93a92
Jiyeon Moon <jiyeonmoon814@gmail.com>
2021-10-03 16:49:39 +0900

    filter posts

--- infinite-scrolling/main.js
+++ infinite-scrolling/main.js
@@ -8,7 +8,14 @@ let page = 1
 document.addEventListener('DOMContentLoaded',() => showPosts())
 window.addEventListener('scroll', () => {
     const { scrollTop, scrollHeight, clientHeight } = document.documentElement
-
+    // scrollTop is getting the number of pixels scrolled 
+    
+    // scrollHeight is a measurement of the height of an element's content,
+    // including content not visible on the screen due to overflow 
+    
+    // clientHeight is zero for elements with no CSS or inline boxes 
+    // it includes padding but excludes borders, margins, and horizontal scrollbars if present.
+    
     if(scrollHeight - scrollTop === clientHeight){
         showLoading()
     }
@@ -18,42 +25,61 @@ window.addEventListener('scroll', () => {
 async function getPosts() {
     const res = await fetch(
         `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
-    )
-
-    const data = await res.json()
-
-    return data 
-}
-
-// Show posts in DOM
-async function showPosts() {
-    const posts = await getPosts()
-
-    posts.forEach(post => {
-        const postEl = document.createElement('div')
-        postEl.classList.add('post')
-        postEl.innerHTML = `
-        <div class="number">${post.id}</div>
-        <div class="post-info">
+        )
+        
+        const data = await res.json()
+        
+        return data 
+    }
+    
+    // Show posts in DOM
+    async function showPosts() {
+        const posts = await getPosts()
+        
+        posts.forEach(post => {
+            const postEl = document.createElement('div')
+            postEl.classList.add('post')
+            postEl.innerHTML = `
+            <div class="number">${post.id}</div>
+            <div class="post-info">
             <h2 class="post-title">${post.title}</h2>
             <p class="post-body">${post.body}</p>
-        </div>
-        `
-
-        postContainer.appendChild(postEl)
-    })
-}
-
-//show and fetch more posts 
-function showLoading() {
-    loading.classList.add('show')
-
-    setTimeout(() => {
-        loading.classList.remove('show')
-
+            </div>
+            `
+            
+            postContainer.appendChild(postEl)
+        })
+    }
+    
+    //show and fetch more posts 
+    function showLoading() {
+        loading.classList.add('show')
+        
         setTimeout(() => {
-            page++
-            showPosts()
-        },300)
-    }, 1000)
-}
+            loading.classList.remove('show')
+            
+            setTimeout(() => {
+                page++
+                showPosts()
+            },300)
+        }, 1000)
+    }
+    
+    // filter posts by search 
+    const filterPosts = e => {
+        const term = e.target.value.toUpperCase()
+        const posts = document.querySelectorAll('.post')
+        
+        posts.forEach(post => {
+            const title = post.querySelector('.post-title').innerText.toUpperCase()
+            const body = post.querySelector('.post-body').innerText.toUpperCase()
+            
+            if(title.indexOf(term) >= 0 || body.indexOf(term) >= 0) {
+                post.style.display = 'flex'
+            }else {
+                post.style.display = 'none'
+            }
+        })
+    }
+
+    document.addEventListener('input', filterPosts)
